b47d264ee084fa8a2e57f7993ab2244b55331914
David C <david94.correa@gmail.com>
2025-01-25 16:28:23 +0100

    first commit

---
+++ src/router/index.ts
@@ -0,0 +1,12 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const routes = [
+  // Agrega tus rutas aquí cuando las necesites
+];
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
+
+export default router;
