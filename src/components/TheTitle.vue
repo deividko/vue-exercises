95a700aa243f155fca8b30be210666ee19127352
David C <david94.correa@gmail.com>
2025-01-25 17:10:36 +0100

    Ejercicio 2 Vue, Crea tu primer componente

---
+++ src/components/TheTitle.vue
@@ -0,0 +1,20 @@
+
+<template>
+  <h2 class="title">Título estático</h2>
+</template>
+
+<script lang="ts">
+export default {
+  name: "TheTitle",
+};
+</script>
+
+<style scoped lang="scss">
+.title {
+  font-size: 34px;
+  color: $primary-color;
+  text-align: center;
+  margin-top: 3em;
+  font-weight: bold;
+}
+</style>
